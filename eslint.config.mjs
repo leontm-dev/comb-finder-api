0faf2cb406e890d328b4f5f259913b52323dfe77
Leon <LAnneken09@gmail.com>
2026-06-18 19:15:27 +0200

    Updated the config files and added a new config for yarn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,46 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+export default defineConfig({
+  files: ['**/*.ts', '**/*.tsx'],
+  languageOptions: {
+    parser: typescriptParser,
+    parserOptions: {
+      project: './tsconfig.json',
+      sourceType: 'module',
+    },
+    globals: {
+      ...globals.node,
+      ...globals.jest,
     },
   },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
-    },
+  plugins: {
+    '@typescript-eslint': typescriptEslintPlugin,
+    'unused-imports': eslintPluginUnusedImports,
   },
-);
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { fixStyle: 'inline-type-imports', prefer: 'type-imports' },
+    ],
+    '@typescript-eslint/no-import-type-side-effects': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_', // Ignoriert Variablen, die mit _ beginnen
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+
+  ignores: ['dist/'],
+});
