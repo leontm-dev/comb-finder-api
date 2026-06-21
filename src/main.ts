0fc1057161bfc1e5e9e501f704de463fd0002491
Leon <LAnneken09@gmail.com>
2026-06-21 11:32:54 +0200

    Activated the logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: [
+      'debug',
+      'log',
+      'error',
+      'error',
+      'warn',
+      'fatal',
+    ],
+  });
   await app.listen(process.env.PORT ?? 9090);
 }
 bootstrap();
