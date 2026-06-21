3938064eac3fefac53c25301bd447da5715e2d67
Leon <LAnneken09@gmail.com>
2026-06-21 10:50:57 +0200

    Added new modules to the app to handle, maps, events, the finder but also new event proposals

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ThrottlerModule } from '@nestjs/throttler';
+
+import { EventsModule } from './events/events.module';
+import { FinderModule } from './finder/finder.module';
+import { MapsModule } from './maps/maps.module';
+import { ProposalsModule } from './proposals/proposals.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      cache: true,
+    }),
+    ScheduleModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL!),
+    EventsModule,
+    ProposalsModule,
+    FinderModule,
+    MapsModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 30,
+        },
+      ],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
