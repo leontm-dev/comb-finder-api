0ec9cff1b0d33a282ca02d1eb1aeec03aaa7f007
Leon <LAnneken09@gmail.com>
2026-06-23 16:56:58 +0200

    Changed the way the ApiResponse object is returned

--- src/interceptors/response/response.interceptor.ts
+++ src/interceptors/response/response.interceptor.ts
@@ -4,7 +4,6 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { Response } from 'express';
 import { map, Observable } from 'rxjs';
 import { ApiResponse } from 'src/types/response.class';
 
@@ -16,19 +15,15 @@ export class ResponseInterceptor implements NestInterceptor {
   ): Observable<any> {
     return next.handle().pipe(
       map((data) => {
-        const response = context
-          .switchToHttp()
-          .getResponse<Response>();
-
         if (data instanceof ApiResponse) {
           return data;
         }
 
-        return response
-          .status(200)
-          .json(
-            new ApiResponse(200, 'Success', data).toJSON(),
-          );
+        return new ApiResponse(
+          200,
+          'Success',
+          data,
+        ).toJSON();
       }),
     );
   }
