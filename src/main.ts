40d01b76fdf2c553e60a8e069ae54c205d0961e3
Leon <LAnneken09@gmail.com>
2026-08-02 23:06:47 +0200

    Added compression, cors configuration and docs

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
+import compression from 'compression';
 
 import { AppModule } from './app.module';
 
@@ -12,7 +18,62 @@ async function bootstrap() {
       'warn',
       'fatal',
     ],
+    cors: {
+      origin: '*',
+      methods: ['GET', 'PATCH'],
+    },
   });
+  app.use(compression());
+  const documentConfig = new DocumentBuilder()
+    .setTitle('comp-finder API')
+    .setDescription(
+      'Find comps with filters or simply by currently trending comps.',
+    )
+    .setVersion('0.1.0')
+    .setContact(
+      'LeonTM',
+      'https://leontm.me',
+      'lanneken09@gmail.com',
+    )
+    .setTermsOfService(
+      'https://github.com/leontm-dev/comp-finder',
+    )
+    .addSecurity('basic-rate-limits', {
+      description:
+        'Rate limits are automatically applied to not run into performance issues.',
+      type: 'http',
+    })
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup(
+    '/docs-swagger',
+    app,
+    documentFactory,
+  );
+  app.use(
+    '/docs',
+    apiReference({
+      content: documentFactory,
+      layout: 'modern',
+      theme: 'fastify',
+      title: 'comp-finder API',
+      showSidebar: true,
+      _integration: 'nestjs',
+      persistAuth: false,
+      documentDownloadType: 'json',
+      darkMode: true,
+      metaData: {
+        title: 'comp-finder API',
+        description:
+          'An api to programmatically use the comp-finder service',
+      },
+      hideDownloadButton: false,
+      withDefaultFonts: true,
+      defaultOpenAllTags: false,
+      slug: 'comp-finder-api',
+    }),
+  );
   await app.listen(process.env.PORT ?? 9090);
 }
 bootstrap();
